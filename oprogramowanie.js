245675098c50b1181d1586eec93552eda30934cd
BartekBuczek <104015906+BartekBuczek@users.noreply.github.com>
2022-04-20 00:37:34 +0200

    Update oprogramowanie.js

--- oprogramowanie.js
+++ oprogramowanie.js
@@ -6,14 +6,14 @@ function blokimenu(){
     blokimenu = "";
     
     for (let iloscblokow = 0; iloscblokow < 5; iloscblokow++) {
-        blokimenu += "<th id='" +  iloscblokow + "' style='left:"+ (iloscblokow*20) +"%;'><cos></th>";
+        blokimenu += "<th id='" +  iloscblokow + "' style='left:"+ (iloscblokow*20) +"%;'></th>";
     }
     var menuu = document.getElementById("tabela");
     menuu.innerHTML += blokimenu;
     var glowna = document.getElementById("0");
     glowna.innerHTML += "<a href='https://bartekbuczekbazadanych.tk/'>Strona główna</a>"
     var aktualnosci = document.getElementById("1");
-    aktualnosci.innerHTML += "<a href='https://bartekbuczekbazadanych.tk/'>Aktualności</a>"
+    aktualnosci.innerHTML += "<a href='https://bartekbuczekbazadanych.tk/aktualnosci.html'>Aktualności</a>"
     var mojedane = document.getElementById("3");
     mojedane.innerHTML += "<a href='https://bartekbuczekbazadanych.tk/'>Moja baza danych</a>"
     var profil = document.getElementById("2");
